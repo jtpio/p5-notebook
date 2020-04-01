01d97ef23e4ae88f733f7a2cf36690ca9618765e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-04-01 21:58:12 +0200

    Add about popup

--- src/app/plugins/top/index.tsx
+++ src/app/plugins/top/index.tsx
@@ -3,9 +3,13 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { showDialog, Dialog } from '@jupyterlab/apputils';
+
 import { LabIcon } from '@jupyterlab/ui-components';
 
 import { Widget } from '@lumino/widgets';
+
+import * as React from 'react';
 
 import { MainMenu } from './menu';
 
@@ -72,11 +76,50 @@ const plugin: JupyterFrontEndPlugin<IMainMenu> = {
       }
     });
 
+    commands.addCommand(CommandIDs.about, {
+      label: `About ${app.name}`,
+      execute: () => {
+        const title = (
+          <span className="p5-about-header">
+            <icon.react margin="7px 9.5px" height="auto" width="58px" />
+            <div className="p5-about-header-info">About the p5 Notebook</div>
+          </span>
+        );
+
+        const p5NotebookURL = 'https://github.com/jtpio/p5-notebook';
+        const externalLinks = (
+          <span>
+            <a
+              href={p5NotebookURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="jp-Button-flat p5-about-externalLinks"
+            >
+              P5 NOTEBOOK ON GITHUB
+            </a>
+          </span>
+        );
+        const body = <div className="p5-about-body">{externalLinks}</div>;
+
+        return showDialog({
+          title,
+          body,
+          buttons: [
+            Dialog.createButton({
+              label: 'Dismiss',
+              className: 'p5-about-button jp-mod-reject jp-mod-styled'
+            })
+          ]
+        });
+      }
+    });
+
     const resourcesGroup = RESOURCES.map(args => ({
       args,
       command: CommandIDs.open
     }));
 
+    menu.helpMenu.addGroup([{ command: CommandIDs.about }]);
     menu.helpMenu.addGroup(resourcesGroup);
 
     app.shell.add(logo, 'top');
