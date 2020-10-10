58205295bf42a8f1046913fc4858d0331658223d
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-10-10 11:52:00 +0200

    Do not stage resources

--- stage.js
+++ stage.js
@@ -5,11 +5,9 @@ const basePath = path.resolve('.');
 const dest = path.resolve(basePath, 'dist');
 fs.ensureDirSync(dest);
 
-['index.html', 'favicon.ico', './build/resources', './build/bundle.js'].forEach(
-  f => {
-    const src = path.resolve(basePath, f);
-    const target = path.resolve(dest, f);
-    console.log(src, target);
-    fs.copySync(src, target);
-  }
-);
+['index.html', 'favicon.ico', './build/bundle.js'].forEach(f => {
+  const src = path.resolve(basePath, f);
+  const target = path.resolve(dest, f);
+  console.log(src, target);
+  fs.copySync(src, target);
+});
