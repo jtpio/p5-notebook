f9a37445c0baed6abb7e1fbdcea8f76832b832ad
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-10-02 11:15:26 +0200

    Fix develop

--- p5_notebook/__init__.py
+++ p5_notebook/__init__.py
@@ -5,7 +5,7 @@ from ._version import __version__
 
 HERE = Path(__file__).parent.resolve()
 
-with (HERE / "packages/lab-extension/package.json").open() as fid:
+with (HERE / "labextension" / "package.json").open() as fid:
     data = json.load(fid)
 
 
