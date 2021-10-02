b7a5f657e65396276fe319f2f579629c9919a3c1
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-10-02 10:13:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ set -xeu
 # bootstrap the environment
 curl -L  https://gist.githubusercontent.com/jtpio/4c184a755c332b35d7de456603eac119/raw/11a54dd5eacc833fafc9e24b5cef18c4d31586dd/bootstrap.sh | bash -
 
+# activate the environment
+micromamba activate
+
 # install dependencies
 python -m pip install -r requirements-deploy.txt
 
