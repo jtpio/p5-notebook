ff8eb3c7726d0327994e9ba7d6c887b41186008d
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-01-31 23:58:45 +0100

    shell hook

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ yum install wget || true
 
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 
+./bin/micromamba shell hook -p ~/micromamba -s posix
+
 ./bin/micromamba shell init -s bash -p ~/micromamba
 
 micromamba activate
