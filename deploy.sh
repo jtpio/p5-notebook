727052ab1124126bdc4b837dc2aa4cc6db7137b5
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-01 10:29:20 +0100

    fix path

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ wget -qO- ${URL} | tar -xvj bin/micromamba
 
 ~/bin/micromamba shell init -s bash -p ~/micromamba
 
-eval "$(micromamba shell hook --shell=)"
+eval "$(~/bin/micromamba shell hook --shell=)"
 
 micromamba activate
 micromamba install python=3.10 -c conda-forge -y
