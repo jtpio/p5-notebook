5f76655fedc9ed8d40c41b8b707b580e650ae208
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-07 10:21:41 +0100

    `export ZSH_VERSION=""`

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ set -xeu
 yum install tar wget || true
 
 cd $HOME
+export ZSH_VERSION=""
 export MAMBA_VERSION=0.20.0
 URL="https://anaconda.org/conda-forge/micromamba/${MAMBA_VERSION}/download/linux-64/micromamba-${MAMBA_VERSION}-0.tar.bz2"
 wget -qO- ${URL} | tar -xvj bin/micromamba
