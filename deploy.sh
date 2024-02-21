56b7a404d9ddd5299e7975bce1ccfc4a85db194e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-02-21 09:56:39 +0100

    Pin jupyterlab for now (#118)
    * Pin jupyterlab for now
    
    * debug
    
    * debug
    
    * dbg
    
    * dbg
    
    * dbg
    
    * dbg
    
    * dbg
    
    * dbg
    
    * dbg
    
    * dbg
    
    * give up

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,12 @@ set -xeu
 
 # install pixi
 export PIXI_VERSION=v0.9.1 && curl -fsSL https://pixi.sh/install.sh | bash
-source ~/.bashrc
 
-pixi install
-pixi run build_lite
-pixi run copy_favicon
+# This started to fail as of February 21st 2024
+# See commit log in https://github.com/jtpio/p5-notebook/pull/118
+# source ~/.bashrc
+
+# So explicitly pointing to the pixi binary instead
+~/.pixi/bin/pixi install
+~/.pixi/bin/pixi run build_lite
+~/.pixi/bin/pixi run copy_favicon
