c9f6a9ac94d2fde1a7d949ced757371b31d37e73
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-02-21 08:27:24 +0000

    dbg

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -xeu
 
 touch ~/.bashrc
+touch ~/.bash_profile
 ls -lisah ~
 
 # install pixi
@@ -11,6 +12,7 @@ export PIXI_VERSION=v0.9.1 && curl -fsSL https://pixi.sh/install.sh | bash
 ls -lisah ~
 echo $(basename "$SHELL")
 source ~/.bashrc
+source ~/.bash_profile
 
 pixi install
 pixi run build_lite
