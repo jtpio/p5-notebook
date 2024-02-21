82c125dccbd3f58ff5e8a71158d0449402c90e09
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-02-21 08:33:02 +0000

    give up

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,14 @@
 # small script to deploy to Vercel
 set -xeu
 
-touch ~/.bashrc
-touch ~/.bash_profile
-ls -lisah ~
-
 # install pixi
 export PIXI_VERSION=v0.9.1 && curl -fsSL https://pixi.sh/install.sh | bash
-ls -lisah ~
-echo $(basename "$SHELL")
-source ~/.bashrc
-source ~/.bash_profile
 
+# This started to fail as of February 21st 2024
+# See commit log in https://github.com/jtpio/p5-notebook/pull/118
+# source ~/.bashrc
+
+# So explicitly pointing to the pixi binary instead
 ~/.pixi/bin/pixi install
 ~/.pixi/bin/pixi run build_lite
 ~/.pixi/bin/pixi run copy_favicon
