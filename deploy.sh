e9e1c37e341786fad2e4897da38315fb6f4297cd
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-02-21 08:25:32 +0000

    dbg

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # small script to deploy to Vercel
 set -xeu
 
+touch ~/.bashrc
+ls -lisah ~
+
 # install pixi
 export PIXI_VERSION=v0.9.1 && curl -fsSL https://pixi.sh/install.sh | bash
 ls -lisah ~
