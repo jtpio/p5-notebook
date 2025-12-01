5f71108384d755b80e5348ec9fc890e10fef4fc8
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-12-01 08:38:01 +0100

    Update deps (#121)
    * Update deps
    
    * bump pixi
    
    * bumps
    
    * fixes
    
    * lock
    
    * prettier
    
    * disable browser check for now
    
    * more fixes
    
    * sync
    
    * fix
    
    * fix

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,11 @@
 # small script to deploy to Vercel
 set -xeu
 
-# install pixi
-export PIXI_VERSION=v0.9.1 && curl -fsSL https://pixi.sh/install.sh | bash
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
-# This started to fail as of February 21st 2024
-# See commit log in https://github.com/jtpio/p5-notebook/pull/118
-# source ~/.bashrc
-
-# So explicitly pointing to the pixi binary instead
-~/.pixi/bin/pixi install
-~/.pixi/bin/pixi run build_lite
-~/.pixi/bin/pixi run copy_favicon
+# install Python and run build commands with uv
+~/.local/bin/uv python install 3.12
+~/.local/bin/uv sync
+~/.local/bin/uv run jupyter lite build
+cp ./favicon.ico ./_output/lab/favicon.ico
